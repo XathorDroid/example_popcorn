4a73f51e8905ea73bdce3637d4fc8e90cecd30c7
Xathor <andymarcel.97@gmail.com>
2021-02-03 22:08:30 +0100

    Add new constants with api URL and model for popular-movies request response.

--- app/src/main/java/com/xathordroid/popcorn/common/Constantes.kt
+++ app/src/main/java/com/xathordroid/popcorn/common/Constantes.kt
@@ -3,6 +3,8 @@ package com.xathordroid.popcorn.common
 class Constantes {
 
     companion object {
-        public val TMDBAPI_BAASE_URL = ""
+        public val TMDBAPI_BAASE_URL = "https://api.themoviedb.org/3"
+        public val API_KEY = ""
+        public val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
     }
 }
